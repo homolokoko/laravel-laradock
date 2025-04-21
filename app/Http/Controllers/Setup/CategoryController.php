e732664d606b536a1fcc6514f143d702c8fb6a18
Hengtong <113340722+homolokoko@users.noreply.github.com>
2025-04-21 23:09:39 +0700

    include category controller

--- app/Http/Controllers/Setup/CategoryController.php
+++ app/Http/Controllers/Setup/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Setup;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Setup;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
